5758ad47880b5f72cb32635e73c1aa69af3c6aae
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-01-07 09:28:09 +0100

    cambiado orden de tablas

--- ejemplo1.sql
+++ ejemplo1.sql
@@ -2,6 +2,15 @@ DROP DATABASE IF EXISTS repasosql;
 CREATE DATABASE repasosql;
 USE repasosql;
 
+CREATE TABLE TDireccion
+(
+    pkDirID INT(4) NOT NULL,
+    Calle VARCHAR(20) NOT NULL,
+    Numero INT(4) NOT NULL,
+    CP INT(5) NOT NULL,
+    Provincia VARCHAR(20) NOT NULL
+);
+
 CREATE TABLE TPersona
 (
     pkID INT(4) NOT NULL PRIMARY KEY,
@@ -11,13 +20,4 @@ CREATE TABLE TPersona
     FechaNac DATE,
     fkDireccionId INT(4) NOT NULL,
     FOREIGN KEY (fkDireccionId) REFERENCES TDireccion(pkDirID)
-);
-
-CREATE TABLE TDireccion
-(
-    pkDirID INT(4) NOT NULL,
-    Calle VARCHAR(20) NOT NULL,
-    Numero INT(4) NOT NULL,
-    CP INT(5) NOT NULL,
-    Provincia VARCHAR(20) NOT NULL
 );
