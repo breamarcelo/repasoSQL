28e6009513d7d79dc608d3f172b4ff4a47a32c61
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-01-07 09:15:45 +0100

    creado Tablas TPersona y TDireccion con pk y fk

--- ejemplo1.sql
+++ ejemplo1.sql
@@ -1,4 +1,25 @@
 DROP DATABASE IF EXISTS repasosql;
 CREATE DATABASE repasosql;
+USE repasosql;
 
+CREATE TABLE TPersona
+(
+    pkID INT(4) NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(20) NOT NULL,
+    Apellidos VARCHAR(20) NOT NULL,
+    Telefono VARCHAR,
+    FechaNac DATE,
+    fkDireccionId INT(4) NOT NULL,
+    FOREIGN KEY (fkDireccionId) REFERENCES TDireccon(pkID) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
+);
 
+CREATE TABLE TDireccon
+(
+    pkID INT(4) NOT NULL,
+    Calle VARCHAR(20) NOT NULL,
+    Numero INT(4) NOT NULL,
+    CP INT(5) NOT NULL,
+    Provincia VARCHAR(20) NOT NULL
+);
