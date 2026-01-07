54069a4f62f2688ca454195a52251ded97665c02
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-01-07 09:30:29 +0100

    correcciones

--- ejemplo1.sql
+++ ejemplo1.sql
@@ -2,9 +2,9 @@ DROP DATABASE IF EXISTS repasosql;
 CREATE DATABASE repasosql;
 USE repasosql;
 
-CREATE TABLE TDireccion
+CREATE TABLE TDireccon
 (
-    pkDirID INT(4) NOT NULL,
+    pkID INT(4) NOT NULL PRIMARY KEY,
     Calle VARCHAR(20) NOT NULL,
     Numero INT(4) NOT NULL,
     CP INT(5) NOT NULL,
@@ -16,8 +16,12 @@ CREATE TABLE TPersona
     pkID INT(4) NOT NULL PRIMARY KEY,
     Nombre VARCHAR(20) NOT NULL,
     Apellidos VARCHAR(20) NOT NULL,
-    Telefono VARCHAR(10),
+    Telefono VARCHAR,
     FechaNac DATE,
     fkDireccionId INT(4) NOT NULL,
-    FOREIGN KEY (fkDireccionId) REFERENCES TDireccion(pkDirID)
+    FOREIGN KEY (fkDireccionId) REFERENCES TDireccon(pkID) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
 );
+
+
