1fce3af43a3ece0d3cb81f97fa9420a964d30f50
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-01-08 16:45:12 +0100

    deleted DEFAULT on pkID

--- ejemplo1.sql
+++ ejemplo1.sql
@@ -4,35 +4,49 @@ USE repasosql;
 
 CREATE TABLE TDireccion
 (
-    pkID INT(4) NOT NULL DEFAULT AUTO_INCREMENT,
-    Calle VARCHAR(20) NOT NULL,
-    Numero INT(4) NOT NULL,
-    CP INT(5) NOT NULL DEFAULT 33401,
-    Provincia VARCHAR(20) NOT NULL,
-    PRIMARY KEY (pkID)
+    pkID INT(4) NOT NULL
+    AUTO_INCREMENT,
+    Calle VARCHAR
+    (20) NOT NULL,
+    Numero INT
+    (4) NOT NULL,
+    CP INT
+    (5) NOT NULL DEFAULT 33401,
+    Provincia VARCHAR
+    (20) NOT NULL,
+    PRIMARY KEY
+    (pkID)
 );
 
-CREATE TABLE TPersona
-(
-    pkID INT(4) NOT NULL PRIMARY KEY DEFAULT AUTO_INCREMENT,
-    Nombre VARCHAR(20) NOT NULL,
-    Apellidos VARCHAR(20) NOT NULL,
-    Telefono VARCHAR(10),
-    FechaNac DATE,
-    fkDireccionId INT(4) NOT NULL,
-    FOREIGN KEY (fkDireccionId) REFERENCES TDireccion(pkID) 
-    ON DELETE CASCADE 
-    ON UPDATE CASCADE
-);
+    CREATE TABLE TPersona
+    (
+        pkID INT(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        Nombre VARCHAR
+    (20) NOT NULL,
+        Apellidos VARCHAR
+    (20) NOT NULL,
+        Telefono VARCHAR
+    (10),
+        FechaNac DATE,
+        fkDireccionId INT
+    (4) NOT NULL,
+        FOREIGN KEY
+    (fkDireccionId) REFERENCES TDireccion
+    (pkID) 
+    ON
+    DELETE CASCADE 
+    ON
+    UPDATE CASCADE
+    );
 
-INSERT INTO TDireccion
-    (Calle, Numero, CP, Provincia)
-VALUES("Alfaraz", 7, 33410, "Asturias");
-INSERT INTO TDireccion
-    (Calle, Numero, CP, Provincia)
-VALUES("Principal", 1, 28003, "Madrid");
-INSERT INTO TDireccion
-    (Calle, Numero, CP, Provincia)
-VALUES("Reyes Magos", 3, 33011, "Asturias");
+    INSERT INTO TDireccion
+        (Calle, Numero, CP, Provincia)
+    VALUES("Alfaraz", 7, 33410, "Asturias");
+    INSERT INTO TDireccion
+        (Calle, Numero, CP, Provincia)
+    VALUES("Principal", 1, 28003, "Madrid");
+    INSERT INTO TDireccion
+        (Calle, Numero, CP, Provincia)
+    VALUES("Reyes Magos", 3, 33011, "Asturias");
 
 
