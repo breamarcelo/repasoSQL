64564ace70ad71770023acff5e5d819975d03705
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-01-08 16:56:38 +0100

    changed - for / in dates

--- ejemplo1.sql
+++ ejemplo1.sql
@@ -29,10 +29,10 @@ INSERT INTO TDireccion(Calle, Numero, CP, Provincia) VALUES("Alfaraz", 7, 33410,
 INSERT INTO TDireccion(Calle, Numero, CP, Provincia) VALUES("Principal", 1, 28003, "Madrid");
 INSERT INTO TDireccion(Calle, Numero, CP, Provincia) VALUES("Reyes Magos", 3, 33011, "Asturias");
 
-INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Marcelo", "Brea Perez", "666554433", 1987-01-16, 1);
-INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Nathalia", "Rodriguez Reyes", "666554433", 1988-04-18, 1);
-INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Loreto Montserrat", "Brea Rodriguez", "666554433", 2019-03-28, 1);
-INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Iago Marcel", "Brea Rodriguez", "666554433", 2025-10-03, 1);
-INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Melchor", "Mago", "666554433", 1900-01-06, 3);
-INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Gaspar", "Mago", "666554433", 1900-01-06, 3);
-INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Baltazar", "Mago", "666554433", 1900-01-06, 3);
+INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Marcelo", "Brea Perez", "666554433", 1987/01/16, 1);
+INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Nathalia", "Rodriguez Reyes", "666554433", 1988/04/18, 1);
+INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Loreto Montserrat", "Brea Rodriguez", "666554433", 2019/03/28, 1);
+INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Iago Marcel", "Brea Rodriguez", "666554433", 2025/10/03, 1);
+INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Melchor", "Mago", "666554433", 1900/01/06, 3);
+INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Gaspar", "Mago", "666554433", 1900/01/06, 3);
+INSERT INTO TPersona(Nombre, Apellidos, Telefono, FechaNac, fkDireccionId) VALUES("Baltazar", "Mago", "666554433", 1900/01/06, 3);
