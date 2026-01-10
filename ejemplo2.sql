c96d350e3cf883a42795acdc081c7017eff183dc
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-01-10 10:45:29 +0100

    created ejemplo2.sql

---
+++ ejemplo2.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS banco;
+CREATE DATABASE banco;
+USE banco;
+
+CREATE TABLE cuentas
+(
+    id INTEGER PRIMARY KEY,
+    saldo DECIMAL(11,2) CHECK (saldo >= 0)
+);
+
+INSERT INTO cuentas
+VALUES(1, 1000);
+INSERT INTO cuentas
+VALUES(2, 2000);
+INSERT INTO cuentas
+VALUES(3, 0);
+
+BEGIN TRANSACTION;
+UPDATE cuentas SET saldo= saldo + 100 WHERE id = 2;
+UPDATE cuentas SET saldo= saldo - 100 WHERE id = 3;
+COMMIT;
